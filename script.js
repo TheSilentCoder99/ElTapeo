7c9bd4de12b321ed7a11e018c61ce0c68adb964a
Alan Sepulveda <alrd1718@gmail.com>
2025-11-14 14:47:15 +0100

    Añadido carroussel de imagenes y cambios de texto

---
+++ script.js
@@ -0,0 +1,25 @@
+
+  const carroussel = document.querySelector('.carroussel');
+  const fotos = document.querySelectorAll('.foto');
+  const izquierda = document.querySelector('.flecha-izquierda');
+  const derecha = document.querySelector('.flecha-derecha');
+  let index = 0;
+
+  function MostrarFotos(i) {
+    if (i < 0){
+        index = fotos.length - 1;
+    }
+    else if (i >= fotos.length){
+        index = 0;
+
+    } else{
+
+        index = i;
+    } 
+
+    carroussel.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  izquierda.addEventListener('click', () => MostrarFotos(index - 1));
+
+  derecha.addEventListener('click', () => MostrarFotos(index + 1));
